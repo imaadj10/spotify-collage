f653db9879a6ff3c01b2481fb2aaf05502c33fea
Imaad Junaiidi <imaadj10@students.cs.ubc.ca>
2022-01-15 18:48:07 -0600

    Testing Accounts

--- spotify_collage/__init__.py
+++ spotify_collage/__init__.py
@@ -1,9 +1,9 @@
 import spotipy
+import spotipy.util as util
 from spotipy.oauth2 import SpotifyOAuth
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="46a88d041bee44a09ec10578836b63bb",
-                                               client_secret="a9d14e08e94748d09a38b21421fa590b",
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="4d206d80d5354c7ca49248eb616d2db9",
+                                               client_secret="29456c8c9b994ccc958a2ac4594857d3",
                                                redirect_uri="http://localhost:8000",
                                                scope="user-library-read"))
-
 
--- spotify_collage/trending.py
+++ spotify_collage/trending.py
@@ -1,17 +1,11 @@
 from spotify_collage import sp
 
+trending_id = "37i9dQZEVXbMDoHDwVN2tF"
+
 def trending():
-    # results = sp.search(q='', limit=20)
-    # for idx, track in enumerate(results['tracks']['items']):
-    #     print(idx, track['name'])
-    #print(sp.playlist_cover_image("37i9dQZEVXbMDoHDwVN2tF"))
-    results = sp.playlist_items("37i9dQZEVXbMDoHDwVN2tF")
-    #print(results)
+    results = sp.playlist_items(trending_id)
     for track in results['items'][:20]:
         print('track    : ' + track['track']['name'])
         print('artist   : ' + track['track']['album']['artists'][0]['name'])
         print('cover art: ' + track['track']['album']['images'][0]['url'])
         print()
-
-
-#"37i9dQZEVXbMDoHDwVN2tF"   # top 50 global
